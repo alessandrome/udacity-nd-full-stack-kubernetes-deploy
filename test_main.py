cf0c2a20d1d24d13e0f4d5989df58bbd84323f4b
alessandrome <mesti.alessandro.git@gmail.com>
2020-01-15 14:59:38 +0100

    Add pre-build test with assert False to test pre build fail on test

--- test_main.py
+++ test_main.py
@@ -23,12 +23,14 @@ def client():
 
 
 def test_health(client):
+    assert False
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
 
 
 def test_auth(client):
+    assert False
     body = {'email': EMAIL,
             'password': PASSWORD}
     response = client.post('/auth', 
