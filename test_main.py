109628416eee7a5e368a6bd5264c4cf67ca3bd74
alessandrome <mesti.alessandro.git@gmail.com>
2020-01-15 15:33:03 +0100

    Remove assert False used to test code pipeline build

--- test_main.py
+++ test_main.py
@@ -23,14 +23,12 @@ def client():
 
 
 def test_health(client):
-    assert False
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
 
 
 def test_auth(client):
-    assert False
     body = {'email': EMAIL,
             'password': PASSWORD}
     response = client.post('/auth', 
